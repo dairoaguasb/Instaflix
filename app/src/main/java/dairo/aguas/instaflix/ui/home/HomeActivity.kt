ab4007df7e703d2214df125960c8e49c8b00692a
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-06-28 13:57:22 -0500

    Added colors and remove toolbar

--- app/src/main/java/dairo/aguas/instaflix/ui/home/HomeActivity.kt
+++ app/src/main/java/dairo/aguas/instaflix/ui/home/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,13 +22,8 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_home)
-
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.navigation_dashboard, R.id.navigation_notifications)
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_home) as NavHostFragment
+        val navController = navHostFragment.navController
         navView.setupWithNavController(navController)
     }
 }
