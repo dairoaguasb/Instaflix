201581b36f1e64d3602af62d3c1486d04c1bcd2e
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-06-28 13:39:22 -0500

    Created navigation bottom and movies/series fragment

---
+++ app/src/main/java/dairo/aguas/instaflix/ui/home/HomeActivity.kt
@@ -0,0 +1,33 @@
+package dairo.aguas.instaflix.ui.home
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import dairo.aguas.instaflix.R
+import dairo.aguas.instaflix.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navigation_dashboard, R.id.navigation_notifications)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/dairo/aguas/instaflix/ui/movies/MoviesViewModel.kt
@@ -0,0 +1,13 @@
+package dairo.aguas.instaflix.ui.movies
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MoviesViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
