10adb586c2ca6c05f6e7222e861c37305d97824e
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-06-28 12:49:12 -0500

    Configure project

---
+++ app/src/main/java/dairo/aguas/instaflix/InstaflixApplication.kt
@@ -0,0 +1,10 @@
+package dairo.aguas.instaflix
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Dairo Aguas B on 28/06/2021.
+ */
+@HiltAndroidApp
+class InstaflixApplication : Application()
