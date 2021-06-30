a07af2a7d6cc58df989bbaec1e3b3c0bdc12c6e4
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-06-30 12:00:07 -0500

    Created NetworkModule

---
+++ app/src/main/java/dairo/aguas/instaflix/di/NetworkModule.kt
@@ -0,0 +1,73 @@
+package dairo.aguas.instaflix.di
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dairo.aguas.instaflix.BuildConfig
+import dairo.aguas.instaflix.data.network.FlowCallAdapterFactory
+import kotlinx.coroutines.Dispatchers
+import okhttp3.Dispatcher
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+/**
+ * Created by Dairo Aguas B on 30/06/2021.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun coroutineDispatcherProvider() = Dispatchers.IO
+
+    @Provides
+    @Singleton
+    fun moshiProvider(): Moshi = Moshi.Builder().build()
+
+    @Provides
+    @Singleton
+    fun httpInterceptor(): Interceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+        return interceptor
+    }
+
+    @Provides
+    @Singleton
+    fun okHttpProvider(interceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .dispatcher(Dispatcher().apply { maxRequests = MAX_REQUEST })
+            .addInterceptor(interceptor)
+            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun retrofitProvider(moshi: Moshi, client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .addCallAdapterFactory(FlowCallAdapterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(client)
+            .baseUrl(BASE_URL)
+            .build()
+    }
+}
+
+private const val MAX_REQUEST = 1
+private const val TIMEOUT = 30L
+private const val BASE_URL = "https://api.themoviedb.org/3/"
