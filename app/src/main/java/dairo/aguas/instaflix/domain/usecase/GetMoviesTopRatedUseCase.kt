095f02b503bca2804c1f80c074efcd634b74f64d
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-06-30 11:30:45 -0500

    Added apiKey in parameter for use cases

--- app/src/main/java/dairo/aguas/instaflix/domain/usecase/GetMoviesTopRatedUseCase.kt
+++ app/src/main/java/dairo/aguas/instaflix/domain/usecase/GetMoviesTopRatedUseCase.kt
@@ -1,6 +1,6 @@
 package dairo.aguas.instaflix.domain.usecase
 
-import dairo.aguas.instaflix.data.model.MoviesDTO
+import dairo.aguas.instaflix.domain.model.Movies
 import dairo.aguas.instaflix.domain.model.Result
 import dairo.aguas.instaflix.domain.repository.MovieRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,5 +10,6 @@ import kotlinx.coroutines.flow.Flow
  */
 class GetMoviesTopRatedUseCase(private val movieRepository: MovieRepository) {
 
-    operator fun invoke(): Flow<Result<MoviesDTO>> = movieRepository.getMoviesTopRated()
+    operator fun invoke(apiKey: String): Flow<Result<Movies>> =
+        movieRepository.getMoviesTopRated(apiKey)
 }
