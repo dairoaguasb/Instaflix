efd6f54d5d2f7e34250bc0787432ada6c9cac3db
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-06-30 15:34:00 -0500

    Created SeriesState and configure viewModel

--- app/src/main/java/dairo/aguas/instaflix/ui/model/SerieViewData.kt
+++ app/src/main/java/dairo/aguas/instaflix/ui/model/SerieViewData.kt
@@ -1,5 +1,7 @@
 package dairo.aguas.instaflix.ui.model
 
+import dairo.aguas.instaflix.domain.model.Serie
+
 /**
  * Created by Dairo Aguas B on 29/06/2021.
  */
@@ -7,4 +9,12 @@ data class SerieViewData(
     val id: Int,
     val posterPath: String,
     val name: String
-)
+) {
+    constructor(serie: Serie) : this(
+        id = serie.id,
+        posterPath = IMAGE_ULR + serie.posterPath,
+        name = serie.title
+    )
+}
+
+private const val IMAGE_ULR = "https://image.tmdb.org/t/p/w185/"
