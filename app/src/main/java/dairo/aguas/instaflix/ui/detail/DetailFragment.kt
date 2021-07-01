8e9e379aa8550c7b13fafa012450db1d26ee3d71
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-07-01 10:24:32 -0500

    Added handleDetailState

--- app/src/main/java/dairo/aguas/instaflix/ui/detail/DetailFragment.kt
+++ app/src/main/java/dairo/aguas/instaflix/ui/detail/DetailFragment.kt
@@ -6,9 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import dairo.aguas.instaflix.R
 import dairo.aguas.instaflix.databinding.FragmentDetailBinding
+import dairo.aguas.instaflix.ui.model.DetailViewData
+import dairo.aguas.instaflix.ui.utils.gone
+import dairo.aguas.instaflix.ui.utils.visible
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
@@ -28,6 +34,7 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getArgsBundle()
+        subscribeToDetailState()
     }
 
     override fun onDestroyView() {
@@ -40,6 +47,24 @@ class DetailFragment : Fragment() {
             when (it.type) {
                 TYPE_MOVIE -> detailViewModel.getMovieDetail(it.id)
                 TYPE_SERIE -> detailViewModel.getSerieDetail(it.id)
+            }
+        }
+    }
+
+    private fun subscribeToDetailState() {
+        detailViewModel.state.onEach(::handleDetailState).launchIn(lifecycleScope)
+    }
+
+    private fun handleDetailState(detailState: DetailState) {
+        when (detailState) {
+            is DetailState.Loading -> {
+                binding.pbLoading.visible()
+            }
+            is DetailState.Success -> {
+                binding.pbLoading.gone()
+            }
+            is DetailState.Error -> {
+                binding.pbLoading.gone()
             }
         }
     }
