8ec239a9be68586286cb1e0af696452ba9e12e8a
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-06-30 21:01:29 -0500

    Get arguments in DetailFragment

--- app/src/main/java/dairo/aguas/instaflix/ui/detail/DetailFragment.kt
+++ app/src/main/java/dairo/aguas/instaflix/ui/detail/DetailFragment.kt
@@ -6,15 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dairo.aguas.instaflix.R
+import dairo.aguas.instaflix.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getArgsBundle()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun getArgsBundle() {
+        DetailFragmentArgs.fromBundle(requireArguments()).let {
+
+        }
     }
 
     companion object {
