c08e59b614410262898c4ef35e557339aa38d412
Dairo Aguas B <ingdairoaguas@gmail.com>
2022-06-07 15:58:42 -0500

    Removiendo el adaptador de flow

--- app/src/main/java/dairo/aguas/instaflix/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/dairo/aguas/instaflix/data/repository/MovieRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package dairo.aguas.instaflix.data.repository
 
 import dairo.aguas.instaflix.data.endpoints.MovieAPI
+import dairo.aguas.instaflix.domain.model.Movie
+import dairo.aguas.instaflix.domain.model.Movies
 import dairo.aguas.instaflix.domain.model.Result
 import dairo.aguas.instaflix.domain.repository.DomainExceptionRepository
 import dairo.aguas.instaflix.domain.repository.MovieRepository
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.flow
 
 /**
  * Created by Dairo Aguas B on 30/06/2021.
@@ -16,31 +18,31 @@ class MovieRepositoryImpl(
     private val domainExceptionRepository: DomainExceptionRepository
 ) : MovieRepository {
 
-    override fun getMoviesPopular() =
-        movieAPI.getMoviesPopular(apiKey).transform { apiResult ->
-            emit(Result.Success(apiResult.toDomainMovies()))
-        }.catch {
-            throw domainExceptionRepository.manageError(it)
-        }
+    override fun getMoviesPopular() = flow<Result<Movies>> {
+        val apiResult = movieAPI.getMoviesPopular(apiKey)
+        emit(Result.Success(apiResult.toDomainMovies()))
+    }.catch {
+        throw domainExceptionRepository.manageError(it)
+    }
 
-    override fun getMoviesUpcoming() =
-        movieAPI.getMoviesUpcoming(apiKey).transform { apiResult ->
-            emit(Result.Success(apiResult.toDomainMovies()))
-        }.catch {
-            throw domainExceptionRepository.manageError(it)
-        }
+    override fun getMoviesUpcoming() = flow<Result<Movies>> {
+        val apiResult = movieAPI.getMoviesUpcoming(apiKey)
+        emit(Result.Success(apiResult.toDomainMovies()))
+    }.catch {
+        throw domainExceptionRepository.manageError(it)
+    }
 
-    override fun getMoviesTopRated() =
-        movieAPI.getMoviesTopRated(apiKey).transform { apiResult ->
-            emit(Result.Success(apiResult.toDomainMovies()))
-        }.catch {
-            throw domainExceptionRepository.manageError(it)
-        }
+    override fun getMoviesTopRated() = flow<Result<Movies>> {
+        val apiResult = movieAPI.getMoviesTopRated(apiKey)
+        emit(Result.Success(apiResult.toDomainMovies()))
+    }.catch {
+        throw domainExceptionRepository.manageError(it)
+    }
 
-    override fun getMovieDetail(id: Int) =
-        movieAPI.getMovieDetail(id, apiKey).transform { apiResult ->
-            emit(Result.Success(apiResult.toDomainMovie()))
-        }.catch {
-            throw domainExceptionRepository.manageError(it)
-        }
+    override fun getMovieDetail(id: Int) = flow<Result<Movie>> {
+        val apiResult = movieAPI.getMovieDetail(id, apiKey)
+        emit(Result.Success(apiResult.toDomainMovie()))
+    }.catch {
+        throw domainExceptionRepository.manageError(it)
+    }
 }
--- app/src/main/java/dairo/aguas/instaflix/data/repository/SeriesRepositoryImpl.kt
+++ app/src/main/java/dairo/aguas/instaflix/data/repository/SeriesRepositoryImpl.kt
@@ -2,10 +2,12 @@ package dairo.aguas.instaflix.data.repository
 
 import dairo.aguas.instaflix.data.endpoints.SerieAPI
 import dairo.aguas.instaflix.domain.model.Result
+import dairo.aguas.instaflix.domain.model.Serie
+import dairo.aguas.instaflix.domain.model.Series
 import dairo.aguas.instaflix.domain.repository.DomainExceptionRepository
 import dairo.aguas.instaflix.domain.repository.SerieRepository
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.flow
 
 /**
  * Created by Dairo Aguas B on 30/06/2021.
@@ -16,31 +18,31 @@ class SeriesRepositoryImpl(
     private val domainExceptionRepository: DomainExceptionRepository
 ) : SerieRepository {
 
-    override fun getSeriesPopular() =
-        serieAPI.getSeriesPopular(apiKey).transform { apiResult ->
-            emit(Result.Success(apiResult.toDomainSeries()))
-        }.catch {
-            throw domainExceptionRepository.manageError(it)
-        }
+    override fun getSeriesPopular() = flow<Result<Series>> {
+        val apiResult = serieAPI.getSeriesPopular(apiKey)
+        emit(Result.Success(apiResult.toDomainSeries()))
+    }.catch {
+        throw domainExceptionRepository.manageError(it)
+    }
 
-    override fun getSeriesOnAir() =
-        serieAPI.getSeriesOnAir(apiKey).transform { apiResult ->
-            emit(Result.Success(apiResult.toDomainSeries()))
-        }.catch {
-            throw domainExceptionRepository.manageError(it)
-        }
+    override fun getSeriesOnAir() = flow<Result<Series>> {
+        val apiResult = serieAPI.getSeriesOnAir(apiKey)
+        emit(Result.Success(apiResult.toDomainSeries()))
+    }.catch {
+        throw domainExceptionRepository.manageError(it)
+    }
 
-    override fun getSeriesTopRated() =
-        serieAPI.getSeriesTopRated(apiKey).transform { apiResult ->
-            emit(Result.Success(apiResult.toDomainSeries()))
-        }.catch {
-            throw domainExceptionRepository.manageError(it)
-        }
+    override fun getSeriesTopRated() = flow<Result<Series>> {
+        val apiResult = serieAPI.getSeriesTopRated(apiKey)
+        emit(Result.Success(apiResult.toDomainSeries()))
+    }.catch {
+        throw domainExceptionRepository.manageError(it)
+    }
 
-    override fun getSerieDetail(id: Int) =
-        serieAPI.getSerieDetail(id, apiKey).transform { apiResult ->
-            emit(Result.Success(apiResult.toDomainSerie()))
-        }.catch {
-            throw domainExceptionRepository.manageError(it)
-        }
+    override fun getSerieDetail(id: Int) = flow<Result<Serie>> {
+        val apiResult = serieAPI.getSerieDetail(id, apiKey)
+        emit(Result.Success(apiResult.toDomainSerie()))
+    }.catch {
+        throw domainExceptionRepository.manageError(it)
+    }
 }
